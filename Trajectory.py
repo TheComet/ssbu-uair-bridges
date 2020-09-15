2bda576a1b4ca977ae35b54930d48b885d9c121f
TheComet <alex.murray@gmx.ch>
2020-09-15 19:22:09 +0200

    Draw and calculate 2 full bridges and beginning of 3rd

--- Trajectory.py
+++ Trajectory.py
@@ -19,26 +19,45 @@ class Trajectory(Updateable):
         points = list(self.points())
         for i, seg in enumerate(points_to_segments(points)):
             if self.calc.uair1 <= i < self.calc.uair1_end:
-                pygame.draw.line(surface, Config.UAIR1_COLOR, seg[0], seg[1], 1)
-            elif self.calc.uair2 <= i < self.calc.uair2_end:
-                pygame.draw.line(surface, Config.UAIR2_COLOR, seg[0], seg[1], 1)
+                color = Config.UAIR1_COLOR
+            elif self.calc.uair2 <= i < self.calc.land1:
+                color = Config.UAIR2_COLOR
+            elif self.calc.uair3 <= i < self.calc.uair3_end:
+                color = Config.UAIR1_COLOR
+            elif self.calc.uair4 <= i < self.calc.land2:
+                color = Config.UAIR2_COLOR
+            elif self.calc.uair5 <= i < self.calc.uair5_end:
+                color = Config.UAIR1_COLOR
             else:
-                pygame.draw.line(surface, Config.TRAJECTORY_COLOR, seg[0], seg[1], 1)
+                color = Config.TRAJECTORY_COLOR
+
+            pygame.draw.line(surface, color, seg[0], seg[1], 1)
 
         for i, point in enumerate(points):
             if self.calc.uair1 <= i <= self.calc.uair1_end:
-                pygame.draw.circle(surface, Config.UAIR1_COLOR, point, 2, 1)
-            elif self.calc.uair2 <= i <= self.calc.uair2_end:
-                pygame.draw.circle(surface, Config.UAIR2_COLOR, point, 2, 1)
+                color = Config.UAIR1_COLOR
+            elif self.calc.uair2 <= i <= self.calc.land1:
+                color = Config.UAIR2_COLOR
+            elif self.calc.uair3 <= i <= self.calc.uair3_end:
+                color = Config.UAIR1_COLOR
+            elif self.calc.uair4 <= i <= self.calc.land2:
+                color = Config.UAIR2_COLOR
+            elif self.calc.uair5 <= i <= self.calc.uair5_end:
+                color = Config.UAIR1_COLOR
             else:
-                pygame.draw.circle(surface, Config.TRAJECTORY_COLOR, point, 2, 1)
+                color = Config.TRAJECTORY_COLOR
 
-            if i == self.calc.hit1 or i == self.calc.hit2:
+            pygame.draw.circle(surface, color, point, 2, 1)
+
+            if i in (self.calc.hit1, self.calc.hit2, self.calc.hit3, self.calc.hit4, self.calc.hit5):
                 pygame.draw.circle(surface, Config.HIT_COLOR, point, 4, 1)
-            elif i == self.calc.ff1:
-                pygame.draw.circle(surface, Config.FF_COLOR, point, 4, 1)
+            if i in (self.calc.ff1, self.calc.ff2):
+                pygame.draw.circle(surface, Config.FF_COLOR, point, 6, 1)
+            if i == self.calc.uair2_ac_start or i == self.calc.uair4_ac_start:
+                pygame.draw.circle(surface, Config.AC_START_COLOR, point, 4, 1)
 
     def points(self):
-        for i in range(self.calc.land1):
+        # first full hop
+        for i in range(self.calc.uair5_end):
             yield int(i * Config.SCALEX + Config.OFFSETX),\
-                  int(self.calc.get_height_at_frame(self.calc.ff1, i) * Config.SCALEY + Config.OFFSETY)
+                  int(self.calc.get_height_at_frame(i) * Config.SCALEY + Config.OFFSETY)
--- UairBridgesCalculator.py
+++ UairBridgesCalculator.py
@@ -7,7 +7,7 @@ UAIR_HITSTUN = 27
 JUMPSQUAT = 3
 UAIR_DURATION = 26
 UAIR_AC = 18
-AC_LANDING_LAG = 4
+FF_LANDING_LAG = 4
 
 
 class UairBridgesCalculator:
@@ -18,6 +18,7 @@ class UairBridgesCalculator:
         self.hit1 = 0
         self.uair2 = 0
         self.uair2_end = 0
+        self.uair2_ac_start = 0
         self.hit2 = 0
         self.ff1 = 0
         self.land1 = 0
@@ -28,16 +29,25 @@ class UairBridgesCalculator:
         self.hit3 = 0
         self.uair4 = 0
         self.uair4_end = 0
+        self.uair4_ac_start = 0
         self.hit4 = 0
         self.ff2 = 0
         self.land2 = 0
 
+        self.fh3 = 0
+        self.uair5 = 0
+        self.uair5_end = 0
+        self.hit5 = 0
+
         # Variables tweakable by the player
         self.uair1_delay = 0
         self.uair2_delay = 0
-        self.fh2_delay = 0
+        self.ff1_delay = 0
+        self.fh2_delay = 2
         self.uair3_delay = 0
         self.uair4_delay = 0
+        self.ff2_delay = 0
+        self.uair5_delay = 0
 
         self.calculate_frames()
 
@@ -46,39 +56,121 @@ class UairBridgesCalculator:
         self.calculate_frames()
 
     def calculate_frames(self):
+
         # Earliest possible input for a fh uair is 4 frames after jump was pressed
         self.uair1 = self.fh1 + JUMPSQUAT + 1 + self.uair1_delay
         self.uair1_end = self.uair1 + UAIR_DURATION - 1
 
-        # First active uair hitbox is frame 4. There are 3 strong hitboxes, 2 weak. Tests show it usually connects 7
+        # First active uair hitbox is frame 4. There are 3 strong hitboxes, 2 weak. Tests show it usually connects
         # 7 frames after input
         self.hit1 = self.uair1 + 7 - 1
 
+        # Pretty much same calculations for uair2
         self.uair2 = self.uair1_end + 1 + self.uair2_delay
         self.uair2_end = self.uair2 + UAIR_DURATION - 1
         self.hit2 = self.uair2 + 7 - 1
 
-        self.ff1, self.land1 = self.calc_optimal_ff_frame(self.ff1, self.uair2)
+        # Find optimal fast fall timing and find landing frame
+        self.ff1 = self.find_optimal_ff_frame(self.uair2)
+        self.land1 = self.find_landing_frame(self.uair2, self.ff1)
+        self.uair2_ac_start = self.uair2 + UAIR_AC - 1
 
-    def get_height_at_frame(self, ff, frame):
-        if frame <= ff:
-            return FH_DATA[frame]
-        return FH_DATA[ff] + (ff - frame) * FAST_FALL_SPEED * GRAVITY
+        self.fh2 = self.land1 + FF_LANDING_LAG
 
-    def find_landing_frame(self, ff, uair2):
+        self.uair3 = self.fh2 + JUMPSQUAT + 1 + self.uair3_delay
+        self.uair3_end = self.uair3 + UAIR_DURATION - 1
+
+        self.hit3 = self.uair3 + 7 - 1
+
+        self.uair4 = self.uair3_end + 1 + self.uair3_delay
+        self.uair4_end = self.uair4 + UAIR_DURATION - 1
+        self.hit4 = self.uair4 + 7 - 1
+
+        self.ff2 = self.find_optimal_ff_frame(self.uair4)
+        self.land2 = self.find_landing_frame(self.uair4, self.ff2)
+        self.uair4_ac_start = self.uair4 + UAIR_AC - 1
+
+        self.fh3 = self.land2 + FF_LANDING_LAG
+
+        self.uair5 = self.fh3 + JUMPSQUAT + 1 + self.uair5_delay
+        self.uair5_end = self.uair5 + UAIR_DURATION - 1
+        self.hit5 = self.uair5 + 7 - 1
+
+    def lookup_fh_data(self, frame):
+        if frame - self.fh1 < len(FH_DATA):
+            return FH_DATA[frame - self.fh1]
+        if frame - self.fh2 < len(FH_DATA):
+            return FH_DATA[frame - self.fh2]
+        return FH_DATA[frame - self.fh3]
+
+    def get_height_at_frame_with_ff(self, frame, ff):
+        if frame < self.fh1 + 3:
+            return 0
+
+        if frame - self.fh1 - 3 < len(FH_DATA):
+            if frame <= ff:
+                return FH_DATA[frame - self.fh1 - 3]
+            return FH_DATA[ff - self.fh1 - 3] + (ff - frame) * FAST_FALL_SPEED * GRAVITY
+
+        # land, delay and jumpsquat
+        if frame < self.fh2 + 3:
+            return 0
+
+        if frame - self.fh2 - 3 < len(FH_DATA):
+            if frame <= ff:
+                return FH_DATA[frame - self.fh2 - 3]
+            height = FH_DATA[ff - self.fh2 - 3] + (ff - frame) * FAST_FALL_SPEED * GRAVITY
+            return height if height > 0 else 0
+
+        # land, delay and jumpsquat
+        if frame < self.fh3 + 3:
+            return 0
+
+        return FH_DATA[frame - self.fh3 - 3]
+
+    def get_height_at_frame(self, frame):
+        if frame < self.fh1 + 3:
+            return 0
+
+        if frame - self.fh1 - 3 < len(FH_DATA):
+            if frame <= self.ff1:
+                return FH_DATA[frame - self.fh1 - 3]
+            height = FH_DATA[self.ff1 - self.fh1 - 3] + (self.ff1 - frame) * FAST_FALL_SPEED * GRAVITY
+            if height > 0:
+                return height
+
+        # land, delay and jumpsquat
+        if frame < self.fh2 + 3:
+            return 0
+
+        if frame - self.fh2 - 3 < len(FH_DATA):
+            if frame <= self.ff2:
+                return FH_DATA[frame - self.fh2 - 3]
+            height = FH_DATA[self.ff2 - self.fh2 - 3] + (self.ff2 - frame) * FAST_FALL_SPEED * GRAVITY
+            if height > 0:
+                return height
+
+        # land, delay and jumpsquat
+        if frame < self.fh3 + 3:
+            return 0
+
+        return FH_DATA[frame - self.fh3 - 3]
+
+    def find_landing_frame(self, uair2, ff):
         land_on = uair2 + 1
-        while self.get_height_at_frame(ff, land_on) > 0:
+        while self.get_height_at_frame_with_ff(land_on, ff) > 0:
             land_on += 1
         return land_on
 
-    def calc_optimal_ff_frame(self, ff, uair2):
+    def find_optimal_ff_frame(self, uair2):
         # just brute force it
-        while self.find_landing_frame(ff, uair2) - uair2 > UAIR_AC - 1:
+        ff = uair2
+        while self.find_landing_frame(uair2, ff) - uair2 > UAIR_AC - 1:
             ff -= 1
-        while self.find_landing_frame(ff, uair2) - uair2 < UAIR_AC - 1:
+        while self.find_landing_frame(uair2, ff) - uair2 < UAIR_AC - 1:
             ff += 1
 
-        return ff, self.find_landing_frame(ff, uair2)
+        return ff
 
 
 GRAVITY = 0.095
@@ -86,11 +178,7 @@ FALL_SPEED = 1.55
 FAST_FALL_SPEED = 2.48
 
 FH_DATA = [
-    0,
-    0,      # frame 1
-    0,      # frame 2
-    0,      # frame 3
-    0.75,   # airborne
+    0.75,   # first airborne frame
     1.25,
     1.65,
     1.95,
@@ -127,7 +215,7 @@ FH_DATA = [
     2.8,
     2.7,
     2.575,
-    2.45,  # linear fall speed starts here approximately (frame 41)
+    2.45,  # linear fall speed starts here approximately (frame 38)
     2.3,
     2.15,
     2.04,
